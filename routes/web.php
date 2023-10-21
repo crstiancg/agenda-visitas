53292b01773bf2a0aa3170a2f3359a9311df825f
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-10-20 23:37:03 -0500

    dependence

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SecretaryController;
-use App\Http\Controllers\SendController;
+// use App\Http\Controllers\SendController;
 use App\Http\Controllers\VisitController;
 use App\Http\Controllers\VisitorController;
 use Illuminate\Support\Facades\Auth;
@@ -30,6 +30,6 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('visits', [VisitController::class, 'index'])->name('visits.index');
 Route::get('visist/pdf', [HomeController::class, 'generatePDF'])->name('home.pdf');
 
-Route::get('/activity', [SendController::class, 'updatedActivity'])->name('activity.updatedActivity');
+// Route::get('/activity', [SendController::class, 'updatedActivity'])->name('activity.updatedActivity');
 
 Auth::routes();
